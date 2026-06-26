2c510db032e76aae0d84e0c2edf912cedd2c0812
Your Name <your-email@example.com>
2026-06-27 01:27:39 +0530

    Fix Orbyo token: fetch dynamically, remove PHP inline include, fix CABINET syntax error in leadAccessToken.php

--- leadAccessToken.php
+++ leadAccessToken.php
@@ -4,7 +4,6 @@
 	$clientDetails->grant_type = "client_credentials";
 	$clientDetails->client_id = "15428";
 	$clientDetails->client_secret = "ktiopma89nmzx8";
-	CABINET
 	$json_data = json_encode($clientDetails);
 	$post = file_get_contents('https://www.orbyo.com/dev/internal/2.3/orbyo/oAuth/token',null,stream_context_create(array(
 	'http' => array(
@@ -22,4 +21,5 @@
 		$post = json_decode($post);
 		$oauthToken = $post->access_token;
 	}
+	echo $oauthToken;
 ?>
